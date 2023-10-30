a87ab7368e7d0f96399bac3088a5880069643377
gggg.gg <noemail@gggg.gg>
2023-10-30 16:50:12 +0800

    tweak install docker in centos init file

--- scripts/ubuntu/init.sh
+++ scripts/ubuntu/init.sh
@@ -5,9 +5,9 @@ sudo apt update
 sudo apt upgrade
 sudo apt full-upgrade
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
