ef2e4fbdac1a7084599048bf19001eb2995745b6
gggg.gg <noemail@gggg.gg>
2023-10-30 15:56:05 +0800

    tweak ubuntu install docker

---
+++ scripts/ubuntu/init.sh
@@ -0,0 +1,13 @@
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+
+sudo apt update
+sudo apt upgrade
+sudo apt full-upgrade
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
