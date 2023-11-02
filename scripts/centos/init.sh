0a21b556e2dfe379636d320dba845b1ede87a938
gggg.gg <noemail@gggg.gg>
2023-11-02 10:02:24 +0800

    tweak install git in centos

--- scripts/centos/init.sh
+++ scripts/centos/init.sh
@@ -1,6 +1,13 @@
+
+# Install Docker and start docker
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-yum makecache --timer
+#yum makecache --timer
 yum -y install docker-ce
 systemctl start docker
+
+
+# Install git
+yum install -y git
+
 
